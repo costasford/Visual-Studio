a20aa40cd3c3a47ad65c1e159db175cadfcc0fd4
Costas Ford <costasford@gmail.com>
2022-06-21 12:52:46 -0700

    initial commit

---
+++ Ford_Costas_JS_Tinkering.js
@@ -0,0 +1,12 @@
+var childHeight = 1
+
+function displayIfChildIsAbleToRideTheRollerCoaster(num1) {
+    if (num1 > 52) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
+
+displayIfChildIsAbleToRideTheRollerCoaster(childHeight)
