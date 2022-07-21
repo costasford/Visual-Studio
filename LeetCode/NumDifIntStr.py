1953acbe0032ee4468e50eb15c9a7ac5b418eac0
Costas Ford <costasford@yahoo.com>
2022-07-21 15:41:39 -0700

    more leetcode

--- LeetCode/NumDifIntStr.py
+++ LeetCode/NumDifIntStr.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Solution:
     def numDifferentIntegers(self, word: str) -> int:
         return len({int(x) for x in re.split(r'[^0-9]', word) if x})
---
+++ LeetCode/PowerofThree.py
@@ -0,0 +1,6 @@
+import math
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        return n>0 and 3**round(math.log(n,3))==n
